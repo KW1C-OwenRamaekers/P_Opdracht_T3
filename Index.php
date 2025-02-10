51f7608a7aa2fefce93277b341fac9f26906eda7
owen-the-kid <owenramaekers2006@gmail.com>
2025-02-10 11:46:16 +0100

    hello

--- Index.php
+++ Index.php
@@ -1,1 +1,14 @@
-<?php
+<html lang="nl">
+<!--
+    Name: Owen Ramaekers
+    Date: 10/02/2025
+-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trainstation073</title>
+</head>
+<body>
+
+</body>
+</html>
