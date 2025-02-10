8c1c7d133c7afc44fa9a8edb3de3d0b5e72bf135
owen-the-kid <owenramaekers2006@gmail.com>
2025-02-10 11:54:38 +0100

    more things

--- Index.php
+++ Index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trainstation073</title>
+    <link rel="stylesheet" href="Styles/Index.css">
 </head>
 <body>
 
